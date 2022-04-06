2f04ad34e8b0676b76e010fa17b0593a360ba4c4
dassokim <kimds0926@gmail.com>
2022-04-06 15:50:55 +0900

    ADD: new data sources url

--- config/openDataConfig.js
+++ config/openDataConfig.js
@@ -1,6 +1,20 @@
 const UKdefaultUrl = "https://data.gov.uk/"
-
 const USAdefaultUrl = "https://catalog.data.gov/"
+const CAdefaultUrl = "https://open.canada.ca/data/en/"
+const CHdefaultUrl = "https://opendata.swiss/en/"
 
 exports.UKdefaultUrl = UKdefaultUrl
 exports.USAdefaultUrl = USAdefaultUrl
+exports.CAdefaultUrl = CAdefaultUrl
+exports.CHdefaultUrl = CHdefaultUrl
+
+/**
+ * ISO_3166_country_codes
+ * 
+ * America: US
+ * Canada: CA
+ * Swiss: CH
+ * Singapore: SG
+ * Korea: KR
+ * exception: UK
+ */
