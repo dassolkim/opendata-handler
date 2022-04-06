a282cea0ba025aa63061ea6122d7c1eff9156825
dassokim <kimds0926@gmail.com>
2022-04-06 20:36:01 +0900

    ADD: read/write urls list in file

--- opendata-handler/fileHandler/file-handler.js
+++ opendata-handler/fileHandler/file-handler.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const dataDir = './data/'
 
-module.exports = {writeFile, readFile}
+module.exports = {writeCatalog, readCatalog, writeUrls, readUrls}
 
-function writeFile(data, sourceInfo) {
+function writeCatalog(data, sourceInfo) {
 
     try{
         const type = sourceInfo.type
@@ -28,7 +28,7 @@ function writeFile(data, sourceInfo) {
 }
 
 
-function readFile(dataDir, sourceInfo) {
+function readCatalog(dataDir, sourceInfo) {
 
     try{
         const type = sourceInfo.type
@@ -37,6 +37,55 @@ function readFile(dataDir, sourceInfo) {
         console.log(exist)
         console.log(`directory path: ${dir}`)
         const file = dir + '/' + sourceInfo.name + '.rdf'
+        console.log(`file path: ${file}`)
+        
+        if(fs.existsSync(file)){
+            const data = fs.readFileSync(file, 'utf-8')
+            console.log(typeof(data))
+            return data
+        } else {
+            return false
+        }
+        
+    } catch (err){
+        console.log(err)
+    }   
+}
+
+function writeUrls(data, sourceInfo) {
+
+    try{
+        const type = sourceInfo.type
+        const dir = dataDir + type
+        console.log(`directory path: ${dir}`)
+        const exist = fs.existsSync(dir)
+        console.log(exist)
+        const file = dir + '/' + sourceInfo.name + '_url.txt'
+        console.log(`file path: ${file}`)
+        const urls = {
+            info: sourceInfo,
+            url: data
+        }
+        fs.writeFileSync(file, JSON.stringify(urls))
+        if(fs.existsSync(file)){
+            return true
+        } else {
+            return false
+        }
+    } catch (err){
+        console.log(err)
+    }   
+}
+
+function readUrls(dataDir, sourceInfo) {
+
+    try{
+        const type = sourceInfo.type
+        const dir = dataDir + type
+        const exist = fs.existsSync(dir)
+        console.log(exist)
+        console.log(`directory path: ${dir}`)
+        const file = dir + '/' + sourceInfo.name + '_url.txt'
         console.log(`file path: ${file}`)
         
         if(fs.existsSync(file)){
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const config = require('./config/openDataConfig')
-const dc = require('./opendata-handler/data-collector')
-const mf = require('./opendata-handler/file-writer')
+const dc = require('./opendata-handler/dataCollector/data-collector')
+const fh = require('./opendata-handler/fileHandler/file-handler')
+const rp = require('./opendata-handler/rdfParser/rdf-parser')
 
 async function main(){
  
@@ -9,27 +10,60 @@ async function main(){
         type: 'catalog',
         name: 'uk_catalog'
     }
-    const USAsourceInfo = {
-        defaultUrl: config.USAdefaultUrl,
+    const USsourceInfo = {
+        defaultUrl: config.USdefaultUrl,
         type: 'catalog',
         name: 'us_catalog'
     }
 
     /**
-     * dstribution/create test
+     * Get RDF Catalog Test
      */
 
-    const catalog = await dc.getCatalog(UKsourceInfo)
-    console.log(`######### Collect ${UKsourceInfo.name} on Web (data portal) #########`)
-    const file = await mf.makeFile(catalog, UKsourceInfo)
+    const catalog = await dc.getCatalog(USsourceInfo)
+    console.log(`######### Collect ${USsourceInfo.name} on Web (data portal) #########`)
+    //console.log(`Get RDF contents: ${catalog}`)
+    //console.log(typeof(catalog)) // string
+    //console.log(JSON.parse(catalog))   // error: < 
+    const file = fh.writeCatalog(catalog, USsourceInfo)
     console.log(file)
     if (file) {
-        console.log('collecting and storing catalog if succeeded')
+        console.log(`collecting and storing ${USsourceInfo.name} file is succeeded`)
     } else{
         console.log('storing file is failed')
     }
-    // console.log(`Get RDF contents: ${catalog}`)
 
+    // UK test file read
+    const dataDir = './data/'
+    const format = 'CSV'
+    const fileData = fh.readCatalog(dataDir, USsourceInfo)
+    
+    if(fileData == false){
+        console.log(`read data is filed`)
+    } else {
+        const parseData = rp.catalogParser(fileData)
+        const dataset = rp.datasetParser(parseData)
+        const csv_urls = rp.distributionParser(parseData, format)
+        //const dist = test['rdf:RDF']['dcat:Distribution']
+        //console.log(JSON.stringify(dist, null, 2))
+        const csv_count = csv_urls.length
+        const dataset_count = dataset.length
+        console.log(`Number of datasets in catalog: ${dataset_count}`)
+        console.log(`number of csv files in catalog: ${csv_count}`)
+        console.log(`csv urls in catalog: ${csv_urls}`)
+        const urlInfo = {
+            name: USsourceInfo.name,
+            type: 'url',
+            format: format,
+            count: csv_count
+        }
+        const wUrls = fh.writeUrls(csv_urls, urlInfo)
+        if(wUrls){
+            console.log(`write urls to files is succeede`)
+        }
+        const rUrls = fh.readUrls(dataDir, urlInfo)
+        console.log(rUrls)
+    }
 }
 if (require.main == module){
     main()
