f95cf9910e8a5a692102b1c417a8458a776b3a28
dassokim <kimds0926@gmail.com>
2022-04-21 14:51:35 +0900

    ADD: get next page catalogs

--- opendata-handler/dataCollector/data-collector.js
+++ opendata-handler/dataCollector/data-collector.js
@@ -1,39 +1,49 @@
 const axios = require('axios').default
 
 // shared api
-module.exports = {getCatalog, getDataset}
-// module.exports = {createSource, getSource, discoverSource, deleteSource}
-function getCatalog(sourceInfo) {
-    const url = sourceInfo.defaultUrl + "catalog.rdf"
-    // const body = {
-    //     workspaceId: sourceInfo.workspaceId,
-    //     sourceDefinitionId: sourceInfo.sourceDefinitionId,
-    //     connectionConfiguration: sourceInfo.connectionConfiguration,
-    //     name: sourceInfo.name
-    // }
-    const result = axios.post(url)
-    .then(function (response){
-        const data = response.data
-        return data
+module.exports = { getCatalog, getNextCatalog, getDataset }
 
-    }).catch(function (error){
-        console.log(error)
-    })
+function getCatalog(sourceInfo) {
+  
+    const url = sourceInfo.defaultUrl + "catalog.rdf"
+
+    const result = axios.post(url)
+        .then(function (response) {
+            const data = response.data
+            return data
+
+        }).catch(function (error) {
+            console.log(error)
+        })
     return result
 }
 
-function getDataset(sourceInfo){
-    const url = sourceInfo.defaultUrl + ".rdf"
-    // const body = {
-    //     sourceId: sourceId
-    // };
-    const result = axios.post(url)
-    .then(function (response){
-        const data = response.data
-        return data
+function getDataset(sourceInfo) {
 
-    }).catch(function (error){
-        console.log(error)
-    })
+    const url = sourceInfo.defaultUrl + ".rdf"
+   
+    const result = axios.post(url)
+        .then(function (response) {
+            const data = response.data
+            return data
+
+        }).catch(function (error) {
+            console.log(error)
+        })
+    return result
+}
+
+function getNextCatalog(sourceInfo, page) {
+
+    const url = sourceInfo.defaultUrl + "catalog.rdf?page=" + page
+
+    const result = axios.post(url)
+        .then(function (response) {
+            const data = response.data
+            return data
+
+        }).catch(function (error) {
+            console.log(error)
+        })
     return result
 }
