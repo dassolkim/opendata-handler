3e6de4190cec2fa4c748fab607d56d82d886ed10
dassokim <kimds0926@gmail.com>
2022-05-30 23:38:38 +0900

    ADD: random extraction with limit

--- opendata-handler/dataExtractor/random-sampling.js
+++ opendata-handler/dataExtractor/random-sampling.js
@@ -1,6 +1,6 @@
 const fh = require('../fileHandler/file-handler')
 
-module.exports = { randomSampling }
+module.exports = { randomSampling, randomSamplingWithLimit }
 
 function randomSampling(end, dataDir, urlInfo) {
 
@@ -20,14 +20,61 @@ function randomSampling(end, dataDir, urlInfo) {
         } else {
             const urlObj = JSON.parse(rUrls)
             const count = urlObj.info.count
-            if (cnt < 2000) {
-                cnt += count
+            if (count == 0) {
+                randomList.pop(random)
             } else {
-                console.log(`Number of files in random list: ${cnt}`)
-                console.log(randomList)
-                break
+                cnt += count
+                if (cnt < 1000) {
+                    // cnt += count
+                    continue
+                } else {
+                    console.log(`Number of files in random list: ${cnt}`)
+                    // console.log(randomList)
+                    break
+                }
             }
         }
     }
+    // console.log(``)
+    return randomList
+}
+
+function randomSamplingWithLimit(end, dataDir, urlInfo, limit) {
+
+    const randomList = []
+    let cnt = 0
+
+    while (true) {
+        const random = Math.floor(Math.random() * end + 1)
+        if (randomList.indexOf(random) === -1) {
+            randomList.push(random)
+        }
+        urlInfo.page = random
+        const rUrls = fh.readUrls(dataDir, urlInfo)
+        if (rUrls == false) {
+            console.log(`Zero ${urlInfo.format} files in catalog page ${urlInfo.page}`)
+            randomList.pop(random)
+        } else {
+            const urlObj = JSON.parse(rUrls)
+            const count = urlObj.info.count
+            if (count == 0) {
+                randomList.pop(random)
+            } else if (count > limit * 1.5 || count < limit * 0.5) {
+                randomList.pop(random)
+            } else {
+                cnt += count
+                if (limit <= cnt <= limit + (limit * 0.2)) {
+                    console.log(`Number of files in random list: ${cnt}`)
+                    break
+                } else {
+                    continue
+                    // randomList.pop(random)
+                    // console.log(randomList)
+                    // break
+                }
+            }
+        }
+    }
+    // console.log(``)
     return randomList
 }
