96e16c30e54cd23a2f39edcf25901664d0185b03
dassokim <kimds0926@gmail.com>
2022-08-05 18:58:07 +0900

    ADD: comparative evaluation with CKAN

---
+++ opendata-handler/tests/Demeter_vs_CKAN/integration-clickhouse.js
@@ -0,0 +1,97 @@
+const configInfo = require('../../../config/connectConfig')
+const create = require('../../../airbyte-api-module/distribution/create/odl')
+const fh = require('../../fileHandler/file-handler')
+const path = require('path')
+const defaultPath = path.join('C:/Users/kimds/nodeProject', 'data/')
+
+
+const destinationInfo = {
+    defaultUrl: configInfo.defaultUrl,
+    connectionConfiguration: configInfo.connectDestination,
+    workspaceId: configInfo.workspaceId,
+    destinationDefinitionId: configInfo.destinationDefinitionId,
+    exist: true,
+    destinationId: "d3369f10-c56f-4d74-9bf3-0efee63353b9", //HPC02 clickhouse
+    name: "us_DST_1K_destination"
+}
+const connectionInfo = {
+    defaultUrl: configInfo.defaultUrl,
+    status: configInfo.status,
+    operationId: configInfo.operationId,
+    sync: true
+}
+
+async function main() {
+
+    let sourceInfo = {
+        defaultUrl: configInfo.defaultUrl
+    }
+    /**
+     * odl-source/validation logic
+     */
+    // read urls in file
+    const dataDir = defaultPath
+    const format = 'csv'
+    const publisher = 'US_with_CE'
+    const page = 1
+    // const lastPage = 3435
+
+    const urlInfo = {
+        name: 'us_catalog',
+        type: 'url',
+        format: format,
+        publisher: publisher,
+        page: page
+    }
+    urlInfo.dirType = 16
+    const f_list = fh.readDirs(dataDir, urlInfo)
+    const length = f_list.length
+
+    console.time(`Time check for ${publisher} portal migration`)
+    let p = 0
+    let global_cnt = 0
+    let global_fcnt = 0
+    while (p < length) {
+        urlInfo.page = f_list[p] // modify
+        const rSources = fh.readIdFile(dataDir, urlInfo, f_list[p])
+        if (rSources == false) {
+            // continue
+            console.log(`${p} catalogs does not contain ${format} files`)
+        } else {
+            const sourceObj = JSON.parse(rSources)
+            const count = sourceObj.info.count
+            console.log(`Number of ${format} file in ${publisher} portal catalog page ${urlInfo.page}: ${count}`)
+
+            let i = 0
+            let cnt = 0
+            let fcnt = 0
+            while (i < count) {
+                const source = sourceObj.sourceList[i]
+                if (source != undefined) {
+                    // console.log(source)
+                    sourceInfo.sourceId = source
+                    const checkResult = await create.createWithSource(sourceInfo, destinationInfo, connectionInfo)
+
+                    if (checkResult == true) {
+                        cnt++
+                        console.log("US DST: connection succeeded")
+                    } else {
+                        fcnt++
+                        console.log("US DST: connection failed")
+                    }
+                }
+                i++
+            }
+            global_cnt += cnt
+            global_fcnt += fcnt
+            console.log(`create and sync connection for ${publisher} workspace, #of connected sources: ${cnt}`)
+        }
+        p++
+    }
+    console.log(`Number of total succeeded migration: ${global_cnt}`)
+    console.log(`Number of total failed migration: ${global_fcnt}`)
+    console.timeEnd(`Time check for ${publisher} portal migration`)
+}
+if (require.main == module) {
+    main()
+}
---
+++ opendata-handler/tests/Demeter_vs_CKAN/validation.js
@@ -0,0 +1,100 @@
+const configInfo = require('../../../config/connectConfig')
+const validate = require('../../../airbyte-api-module/distribution/validate/odl')
+
+const fh = require('../../fileHandler/file-handler')
+const path = require('path')
+const defaultPath = path.join('C:/Users/kimds/nodeProject', 'data/')
+
+async function main() {
+
+    let csvConnectSource = {
+        "url": "https://data.cityofnewyork.us/api/views/wutj-3rsj/rows.csv?accessType=DOWNLOAD",
+        "format": "csv",
+        "provider": {
+            "storage": "HTTPS"
+        },
+        "dataset_name": "ny_portal_csv_test"
+    }
+
+    let csvSourceInfo = {
+        defaultUrl: configInfo.defaultUrl,
+        connectionConfiguration: csvConnectSource,
+        workspaceId: configInfo.workspaceId,
+        sourceDefinitionId: configInfo.csvSourceDefinitnionId,
+    }
+
+    /**
+     * odl-source/validation logic
+     */
+    // read urls in file
+    const dataDir = defaultPath
+    const format = 'csv'
+    const publisher = 'US_with_CE'
+    const page = 263
+    const urlInfo = {
+        name: 'us_catalog',
+        type: 'url',
+        format: format,
+        publisher: publisher,
+        page: page
+    }
+    const lastPage = 263 // 100
+    console.time('Time check for US portal validation with pagenation')
+    let global_cnt = 0
+    let original_cnt = 0
+    let p = page
+    while (p <= lastPage) {
+        const name = `us_p${p}_${format}_`
+        // distribution name extraction
+        urlInfo.page = p
+        const rUrls = fh.readUrls(dataDir, urlInfo)
+        if (rUrls == false) {
+            console.log(`Zero ${format} files in catalog page ${p}`)
+        } else {
+            const urlObj = JSON.parse(rUrls)
+            const count = urlObj.info.count
+            console.log(`Number of ${format} file in ${publisher} portal catalog page ${urlInfo.page}: ${count}`)
+            let i = 0
+            let j = 0
+            let cnt = 0
+            const sourceList = []
+            // console.time('Time check for US portal validation')
+            original_cnt += count
+            while (i < count) {
+                const url = urlObj.url[i]
+                j = i + 1
+                csvConnectSource.url = url
+                csvConnectSource.dataset_name = name + j
+                csvSourceInfo.name = name + j
+
+                const sourceId = await validate.validateWithoutDiscover(csvSourceInfo)
+                if (sourceId == null) {
+                    console.log("odlSource/validatdion failed")
+                } else {
+                    sourceList.push(sourceId)
+                    cnt++
+                    // console.log("odlSource/validation succeeded")
+                }
+                i++
+            }
+            // console.timeEnd('Time check for US portal validation')
+            urlInfo.count = cnt
+            global_cnt += cnt
+            urlInfo.dataType = 'source'
+            urlInfo.dirType = 263
+            const writeSource = fh.writeSourceIds(sourceList, urlInfo)
+            console.log(`create and validate for ODL data service activity`)
+            console.log(`number of created sources in ${publisher}: ${cnt}`)
+            console.log(writeSource)
+        }
+        p++
+    }
+    console.timeEnd('Time check for US portal validation with pagenation')
+    console.log(`Number of ${format} files in US portal: ${original_cnt}`)
+    console.log(`Number of created sources in US portal ${format} files: ${global_cnt}`)
+    // console.log(`(${global_cnt}/${original_cnt}) * 100 %`)
+
+}
+if (require.main == module) {
+    main()
+}
